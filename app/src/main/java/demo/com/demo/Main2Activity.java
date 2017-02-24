a74fb7f40c74dab2e038a3b9a6e95d0bbcf2705d
pratikdeogade <pratikdkte@gmail.com>
2017-02-24 14:49:07 +0530

    first demo push

--- app/src/main/java/demo/com/demo/Main2Activity.java
+++ app/src/main/java/demo/com/demo/Main2Activity.java
@@ -1,13 +1,29 @@
 package demo.com.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.lay_coor);
+        lv= (ListView) findViewById(R.id.lv);
+        ArrayList<String> arrayList=new ArrayList();
+        for (int i = 0; i < 100; i++) {
+            arrayList.add("abc");
+        }
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        lv.setAdapter(arrayAdapter);
     }
+
+
+
 }
