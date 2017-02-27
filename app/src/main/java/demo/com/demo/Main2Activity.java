02f4687677d77d5ae7495bf438b8ce7bf93fc105
pratikdeogade <pratikdkte@gmail.com>
2017-02-27 11:56:36 +0530

    line added pratik

--- app/src/main/java/demo/com/demo/Main2Activity.java
+++ app/src/main/java/demo/com/demo/Main2Activity.java
@@ -2,7 +2,11 @@ package demo.com.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,6 +17,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("pratik");
         setContentView(R.layout.lay_coor);
         lv= (ListView) findViewById(R.id.lv);
         ArrayList<String> arrayList=new ArrayList();
@@ -23,8 +28,37 @@ public class Main2Activity extends AppCompatActivity {
 
         ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
         lv.setAdapter(arrayAdapter);
+        setListViewHeightBasedOnChildren(lv);
+
     }
 
+    public static void setListViewHeightBasedOnChildren(final ListView listView) {
+        listView.post(new Runnable() {
+            @Override
+            public void run() {
+                ListAdapter listAdapter = listView.getAdapter();
+                if (listAdapter == null) {
+                    return;
+                }
+                int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
+                int listWidth = listView.getMeasuredWidth();
+                for (int i = 0; i < listAdapter.getCount(); i++) {
+                    View listItem = listAdapter.getView(i, null, listView);
+                    listItem.measure(
+                            View.MeasureSpec.makeMeasureSpec(listWidth, View.MeasureSpec.EXACTLY),
+                            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
 
+
+                    totalHeight += listItem.getMeasuredHeight();
+                    Log.d("listItemHeight" + listItem.getMeasuredHeight(), "___________");
+                }
+                ViewGroup.LayoutParams params = listView.getLayoutParams();
+                params.height = (int) ((totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1))));
+                listView.setLayoutParams(params);
+                listView.requestLayout();
+
+            }
+        });
+    }
 
 }
